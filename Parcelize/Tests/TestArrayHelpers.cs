9f94d85fd8c20d28d6586d4efdd74d226cc67ecb
Zintom <tzsinkoapps@gmail.com>
2021-03-31 15:49:42 +0100

    Create tests

---
+++ Parcelize/Tests/TestArrayHelpers.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zintom.Parcelize.Tests
+{
+    [TestClass]
+    public class TestArrayHelpers
+    {
+        [TestMethod]
+        public void CombineArrays()
+        {
+            byte[] one = new byte[1] { 1 };
+            byte[] two = new byte[1] { 2 };
+            byte[] three = new byte[1] { 3 };
+
+            byte[] combined = ArrayHelpers.CombineArrays(one, two, three);
+
+            Assert.IsTrue(combined[0] == 1);
+            Assert.IsTrue(combined[1] == 2);
+            Assert.IsTrue(combined[2] == 3);
+            Assert.IsTrue(combined.Length == 3);
+        }
+    }
+}
